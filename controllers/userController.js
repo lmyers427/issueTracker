a505ef6a90b7a728489247165158c18157cbf9c4
Lauren Myers <lmmyers2@mail.fhsu.edu>
2023-03-04 15:34:50 -0700

    Update user profile user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,14 +39,14 @@ const updateUser = async (req, res) => {
     if(req.body.email) user.email = req.body.email;
     if(req.body.first_name) user.first_name = req.body.first_name;
     if(req.body.last_name) user.last_name = req.body.last_name;
-    if(req.body.password) user.password = req.body.password;
+    if(req.body.password2) user.password = req.body.password2;
     if(req.body.street) user.address.street = req.body.street;
     if(req.body.city) user.address.city = req.body.city;
     if(req.body.state) user.address.state = req.body.state;
     
     
 
-    const result = await user.save();
+    //const result = await user.save();
 
     
 
