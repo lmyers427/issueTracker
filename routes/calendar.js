64ace8ee247d9dc08fb6c264a37a60eea68ecbcd
Lauren Myers <lmmyers2@mail.fhsu.edu>
2023-04-26 10:11:39 -0600

    got Ticket Controller and Detail working

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const ticketController = require('../controllers/ticketController')
 
 
 
@@ -12,11 +11,9 @@ router.get('/', (req, res) => {
     //checks if session user has been assigned or if user has logged in. If no, routes user back to login page to login first
     if(!req.session.user || !req.session.role) return res.render(path.join(__dirname, '..', 'views', 'login'), {message: "Please Login"} );
 
-    res.render(path.join(__dirname, '..', 'views', 'createTicket'), {message: ' ', users: req.session.users, teams:req.session.teams}); //with ejs updated to render
+    
+    res.render(path.join(__dirname, '..', 'views', 'calendar'), {message: ' '}); //with ejs updated to render
     
 });
-
-router.post('/' /* , upload.single('screenShot')*/, ticketController.newTicket);
-
 
 module.exports = router;
--- routes/tickets.js
+++ routes/tickets.js
@@ -7,15 +7,7 @@ const ticketController = require('../controllers/ticketController')
 
 
 
-router.get('/', (req, res) => {
-
-    //checks if session user has been assigned or if user has logged in. If no, routes user back to login page to login first
-    if(!req.session.user || !req.session.role) return res.render(path.join(__dirname, '..', 'views', 'login'), {message: "Please Login"} );
-
-    console.log(req.session.users);
-    res.render(path.join(__dirname, '..', 'views', 'tickets'), {message: ' ', users: req.session.users, teams:req.session.teams, imagePath:req.session.imagePath, userDetails: req.session.userDetails}); //with ejs updated to render
-    
-});
+router.get('/', ticketController.getTickets);
 
 router.post('/' /* , upload.single('screenShot')*/, ticketController.newTicket);
 
