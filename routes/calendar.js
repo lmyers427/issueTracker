be0f1512926078501824d75724ad651f00b81bdc
tomscott0 <tonepup@gmail.com>
2023-05-01 10:15:06 -0700

    added includes for views -shared code

--- routes/calendar.js
+++ routes/calendar.js
@@ -12,8 +12,8 @@ router.get('/', (req, res) => {
     if(!req.session.user || !req.session.role) return res.render(path.join(__dirname, '..', 'views', 'login'), {message: "Please Login"} );
 
     
-    res.render(path.join(__dirname, '..', 'views', 'calendar'), {message: ' '}); //with ejs updated to render
-    
+    //res.render(path.join(__dirname, '..', 'views', 'calendar'), {message: ' '}); //with ejs updated to render
+    res.render('../views/calendar', { user: req.session.user, message: '', userDetails: req.session.userDetails, imagePath: req.session.imagePath});
 });
 
 module.exports = router;
